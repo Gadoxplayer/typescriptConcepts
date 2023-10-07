3985b6b876e6ad386cfc03e1d305288164cc633f
EduardoCampo <eduardocampo91@gmail.com>
2023-10-07 16:46:16 -0500

    modifying ts config file

--- basic/app.js
+++ basic/app.js
@@ -1,15 +1,41 @@
 "use strict";
-const msg = "Hello world";
-const hero = {
-    name: "Trapito",
-    age: 4,
+const batimovil = {
+    carroceria: "Negra",
+    modelo: "6x6",
+    antibalas: true,
+    pasajeros: 4
 };
-let b = 10;
-const c = 10;
-let d = 10;
-function sayHello(msg) {
-    console.log(msg);
-}
-(() => {
-    const a = 10;
-})();
+const bumblebee = {
+    carroceria: "Amarillo con negro",
+    modelo: "4x2",
+    antibalas: true,
+    pasajeros: 4,
+    disparar() {
+        console.log("Disparando");
+    }
+};
+const villanos = [{
+        nombre: "Lex Luthor",
+        edad: 54,
+        mutante: false
+    }, {
+        nombre: "Erik Magnus Lehnsherr",
+        edad: 49,
+        mutante: true
+    }, {
+        nombre: "James Logan",
+        edad: undefined,
+        mutante: true
+    }];
+const charles = {
+    poder: "psiquico",
+    estatura: 1.78
+};
+const apocalipsis = {
+    lider: true,
+    miembros: ["Magneto", "Tormenta", "Psylocke", "Angel"]
+};
+let mystique;
+mystique = charles;
+mystique = apocalipsis;
+//# sourceMappingURL=app.js.map
